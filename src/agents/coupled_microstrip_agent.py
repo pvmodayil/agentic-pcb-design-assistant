c36979013453ab12c604774be6d7a957bc78bac3
Philip Modayil <philipv.modayil@gmail.com>
2026-03-16 11:49:52 +0100

    Refactor: Remove unused imports

--- src/agents/coupled_microstrip_agent.py
+++ src/agents/coupled_microstrip_agent.py
@@ -6,10 +6,10 @@ for the given target impedance. Verifications will be done using simulations wit
 """
 import yaml
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any
 from src.core.tool_registry import ToolRegistry
 from src.core.pcb_agent import PCBAgent
-from src.core.data_models import ActionResult, Checkpoint, WorkflowResult
+from src.core.data_models import  Checkpoint, WorkflowResult
 
 from src.tools import coupled_microstrip_parameter_optimizer_tool as cmpo_tool
 from src.tools import bem_field_solver_simulator as bfs_tool
