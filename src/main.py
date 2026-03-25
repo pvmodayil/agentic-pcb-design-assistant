9f9f13da9d3c56e14ea3ea9f8e3fda0391f531e1
Philip Modayil <philipv.modayil@gmail.com>
2026-03-25 12:19:41 +0100

    Feature: Add input query

--- src/main.py
+++ src/main.py
@@ -4,9 +4,22 @@ from src.core.data_models import WorkflowResult
 
 
 async def main() -> None:
-    query: str = ""
+    query: str = """
+    I would like to design a coupled microstrip arrangement to obtain a **target impedance: 90 Ohms**.
+    The fixed parametrs for the arrangement are:
+    material with a **dielectric constant: 4.5**,
+    **height of the arrangement: 250 micro meters**,
+    **thickness of the strip: 35 micro meters**,
+    and **spacing range in micro meters: (150, 300)**
+    
+    Use the provided optimisation tools to optimise the geometric parameters for the coupled microstrip arrangement.
+    Return the final design after optimisation.
+    """
+    
     workflow_result: WorkflowResult = await cmsAgent.run_coupled_microstrip_agent(query=query)
-
+    print("\n" + "="*80)
+    print("Final workflow results")
+    print("="*80)
     print(workflow_result)
     
 if __name__ == "__main__":
